6dade1c9b121f5cdf1a67258e167aa01ea8ee8d1
Mateus Rodrigues <125597047+mateusrodrigues0904@users.noreply.github.com>
2023-09-07 18:02:12 -0300

    Versão final

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,26 +1,76 @@
-let txtcalc = document.getElementById("txtCalc");
 let calc = 0;
 let arrayCalc = [];
+let condOp = "";
+
+let txtcalc = document.getElementById("txtCalc");
 let resultado = document.getElementById("ph19");
 let adicao = document.getElementById("adicao");
 let subtracao = document.getElementById("subtracao");
 let multiplicacao = document.getElementById("multiplicacao");
+let divisao = document.getElementById("divisao");
+
+let index;
+let numNeg;
+let porCem;
 
 function AC() {
+  txtcalc.style.fontSize = "50px";
+  txtcalc.style.marginBottom = "0px";
   txtcalc.innerHTML = 0;
   calc = 0;
   arrayCalc = [];
   arrayCalc.length = 0;
 }
 
+function porcem() {
+  porCem = calc / 100;
+  calc = porCem;
+  let porCemf = String(porCem);
+  if (porCemf.length > 9) {
+    txtcalc.style.fontSize = "23px";
+    txtcalc.style.marginBottom = "30px";
+    txtcalc.innerText = porCem;
+  } else {
+    txtcalc.style.fontSize = "50px";
+    txtcalc.style.marginBottom = "0px";
+    txtcalc.innerText = porCem;
+  }
+}
+
+function negpos() {
+  numNeg = calc * -1;
+  txtcalc.innerHTML = numNeg;
+  calc = numNeg;
+}
+
+function virgula() {
+  if (txtcalc.innerText.indexOf(".") === -1) {
+    txtcalc.innerText += ".";
+    calc = txtcalc.innerText;
+  }
+}
+
 function pegaNumero(n) {
-  if (txtcalc.innerText == 0) {
+  if (txtcalc.innerText == "0") {
     txtcalc.innerText = n;
   } else if (txtcalc.innerText.length >= 9) {
+  } else if (txtcalc.innerText == "0.") {
+    txtcalc.innerText += n;
   } else {
     txtcalc.innerText += n;
   }
   calc = Number(txtcalc.innerText);
+}
+function operacoes(n) {
+  txtcalc.style.fontSize = "50px";
+  condOp = n;
+  txtcalc.innerText = "";
+  arrayCalc.push(calc);
+  calc = null;
+  index = arrayCalc.indexOf(null);
+  if (index > -1) {
+    arrayCalc.splice(index, 1);
+  }
 }
 
 function zero() {
@@ -54,66 +104,108 @@ function nove() {
   pegaNumero(9);
 }
 
-function virgula() {
-  if (txtcalc.innerText.indexOf(".") === -1) {
-    txtcalc.innerText += ".";
-    calc = txtcalc.innerText;
-  }
-}
-
-let condOp = "";
-
 adicao.addEventListener("click", () => {
-  condOp = "adicao";
-  txtcalc.innerText = "";
-  arrayCalc.push(calc);
-  let soma = 0;
-  for (let i = 0; i < arrayCalc.length; i++) {
-    soma += arrayCalc[i];
-  }
+  operacoes("adicao");
 });
-
 subtracao.addEventListener("click", () => {
-  condOp = "subtracao";
-  txtcalc.innerText = "";
-  arrayCalc.push(calc);
-  let sub = 0;
-  for (let i2 = 0; i2 < arrayCalc.length; i2++) {
-    arrayCalc[i2] -= sub;
-  }
+  operacoes("subtracao");
 });
-
 multiplicacao.addEventListener("click", () => {
-  condOp = "multiplicacao";
-  txtcalc.innerText = "";
-  arrayCalc.push(calc);
-  let mult = 1;
-  for (let i3 = 0; i3 < arrayCalc.length; i3++) {
-    mult *= arrayCalc[i3];
-  }
+  operacoes("multiplicacao");
+});
+divisao.addEventListener("click", () => {
+  operacoes("divisao");
 });
 
 resultado.addEventListener("click", () => {
   if (condOp === "adicao") {
     arrayCalc.push(calc);
+    calc = null;
+    index = arrayCalc.indexOf(null);
+    if (index > -1) {
+      arrayCalc.splice(index, 1);
+    }
     let soma = 0;
     for (let i = 0; i < arrayCalc.length; i++) {
       soma += arrayCalc[i];
     }
+    arrayCalc = [];
+    arrayCalc.push(soma);
     txtcalc.innerText = soma;
+    let somaf = String(soma);
+    if (somaf.length > 9) {
+      txtcalc.style.fontSize = "23px";
+      txtcalc.style.marginBottom = "30px";
+      txtcalc.innerText = soma;
+    } else {
+      txtcalc.innerText = soma;
+    }
   } else if (condOp === "subtracao") {
     arrayCalc.push(calc);
-    let sub = arrayCalc[0]; // Comece com o primeiro valor no array
+    calc = null;
+    index = arrayCalc.indexOf(null);
+    if (index > -1) {
+      arrayCalc.splice(index, 1);
+    }
+    let sub = arrayCalc[0];
     for (let i = 1; i < arrayCalc.length; i++) {
       sub -= arrayCalc[i];
     }
-    txtcalc.innerText = sub;
+    arrayCalc = [];
+    arrayCalc.push(sub);
+    let subf = String(sub);
+    if (subf.length > 9) {
+      txtcalc.style.fontSize = "23px";
+      txtcalc.style.marginBottom = "30px";
+      txtcalc.innerText = sub;
+    } else {
+      txtcalc.innerText = sub;
+    }
   } else if (condOp === "multiplicacao") {
     arrayCalc.push(calc);
-    let mult = 1;
-    for (let i3 = 0; i3 < arrayCalc.length; i3++) {
-      mult *= arrayCalc[i3];
+    calc = null;
+    index = arrayCalc.indexOf(null);
+    if (index > -1) {
+      arrayCalc.splice(index, 1);
     }
-    txtcalc.innerHTML = mult;
+    let mult = 1;
+    for (let i = 0; i < arrayCalc.length; i++) {
+      mult *= arrayCalc[i];
+    }
+    arrayCalc = [];
+    arrayCalc.push(mult);
+    let multf = String(mult);
+    if (multf.length > 9) {
+      txtcalc.style.fontSize = "23px";
+      txtcalc.style.marginBottom = "30px";
+      txtcalc.innerText = mult;
+    } else {
+      txtcalc.innerText = mult;
+    }
+  } else if (condOp === "divisao") {
+    arrayCalc.push(calc);
+    calc = null;
+    index = arrayCalc.indexOf(null);
+    if (index > -1) {
+      arrayCalc.splice(index, 1);
+    }
+    let div = arrayCalc[0];
+    for (let i = 1; i < arrayCalc.length; i++) {
+      if (arrayCalc[i] !== 0) {
+        div /= arrayCalc[i];
+      } else {
+        continue;
+      }
+    }
+    arrayCalc = [];
+    arrayCalc.push(div);
+    let divf = String(div);
+    if (divf.length > 9) {
+      txtcalc.style.fontSize = "23px";
+      txtcalc.style.marginBottom = "30px";
+      txtcalc.innerText = div;
+    } else {
+      txtcalc.innerText = div;
+    }
   }
 });
